d695f0c7804ed66bb060f25eebb12f48fdfe55cb
Ryan Bascos <rbascos@uci.edu>
2024-04-30 21:40:10 -0700

    Unshaded Textures
    Made bullets and the eyes of all enemies unshaded, making them look like they glow in the dark.

--- icon/icon.gd
+++ icon/icon.gd
@@ -22,7 +22,7 @@ func _physics_process(_delta):
 		global_position += dir_to_cotu * (dist_to_cotu / 4)
 		if dist_to_cotu > floored_proximity:
 			look_at(cotu.global_position)
-	anim_tree.set("parameters/StateMachine/CotuGroundedBlendSpace/blend_position", dist_to_cotu)
+	anim_tree.set("parameters/StateMachine/CotuGroundedBlendSpace/blend_position", cotu.velocity.length())
 
 func start_following_cotu():
 	following_cotu = true
