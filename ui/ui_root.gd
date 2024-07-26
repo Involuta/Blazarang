773d4371a43e0e3e672142ff785dccf6873671ec
Ryan Bascos <rbascos@uci.edu>
2024-07-25 21:57:39 -0700

    Save Progress before DESTAB Anim
    Will make a DESTABILIZED animation to control shader properties

--- ui/ui_root.gd
+++ ui/ui_root.gd
@@ -24,15 +24,15 @@ func show_destabilized():
 	destab_icon.visible = true
 	var i = 1
 	while i >= 0:
-		destab_shader.set_shader_parameter("opacity", lerp(0.0, .5, i))
+		destab_shader.set_shader_parameter("opacity", lerp(0.15, .5, i))
 		glitch_shader.set_shader_parameter("shake_power", lerp(.005, .03, i))
 		glitch_shader.set_shader_parameter("shake_color_rate", lerp(0.0, .02, i))
 		destab_gradient.modulate = Color(1,1,1,i)
 		await get_tree().create_timer(get_physics_process_delta_time()).timeout
 		i -= .03
+	await get_tree().create_timer(.5).timeout
 	i = 1
 	destab_icon.material = null
-	await get_tree().create_timer(.5).timeout
 	while i >= 0:
 		destab_icon.modulate = Color(1,1,1,i)
 		await get_tree().create_timer(get_physics_process_delta_time()).timeout
