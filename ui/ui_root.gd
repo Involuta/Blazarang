ed6df0d0963bf6c9ba3f3ee4fe8d45f0770f6e77
Ryan Bascos <rbascos@uci.edu>
2024-07-26 09:47:12 -0700

    Destab Animation Polish
    Shortened and polished destabilized animation. Will do gameplay mechanics next

--- ui/ui_root.gd
+++ ui/ui_root.gd
@@ -25,32 +25,10 @@ func _physics_process(delta):
 	cotu_damage_indicator.value = cotu_hurtbox.damage_indicator_value
 	if cotu_hurtbox.destabilized and not destabilized:
 		destab_anim()
-	if (destab_shader != null):
-		destab_shader.set_shader_parameter("opacity", destab_shader_opacity)
+	destab_shader.set_shader_parameter("opacity", destab_shader_opacity)
 	glitch_shader.set_shader_parameter("shake_power", glitch_shader_shake_power)
 	glitch_shader.set_shader_parameter("shake_color_rate", glitch_shader_shake_color_rate)
-
-func show_destabilized():
-	destabilized = true
-	glitch_box.visible = true
-	destab_icon.visible = true
-	var i = 1
-	while i >= 0:
-		destab_gradient.modulate = Color(1,1,1,i)
-		await get_tree().create_timer(get_physics_process_delta_time()).timeout
-		i -= .03
-	await get_tree().create_timer(.5).timeout
-	i = 1
-	destab_icon.material = null
-	while i >= 0:
-		destab_icon.modulate = Color(1,1,1,i)
-		await get_tree().create_timer(get_physics_process_delta_time()).timeout
-		i -= .06
-	destab_icon.visible = false
 
 func destab_anim():
 	destabilized = true
 	$AnimationPlayer.play("destabilize")
-
-func remove_destab_material():
-	destab_icon.material = null
