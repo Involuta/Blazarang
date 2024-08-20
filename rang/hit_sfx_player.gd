b28a5aac777ab74309fb5234a228c6f3f74d30f8
Ryan Bascos <rbascos@uci.edu>
2024-08-19 22:45:57 -0700

    Enemy Hurt SFX Play Properly
    Enemy Hurt sfx used to play whenever it hit any enemy area 3D. It now only plays when hitting an enemy hurtbox, not an enemy physical collider (which collides with the environment).

--- rang/hit_sfx_player.gd
+++ rang/hit_sfx_player.gd
@@ -5,5 +5,6 @@ extends Area3D
 func _ready():
 	area_entered.connect(play_hit_sound)
 
-func play_hit_sound(_area):
-	hit_sfx.play()
+func play_hit_sound(area):
+	if area.name == "EnemyHurtbox":
+		hit_sfx.play()
