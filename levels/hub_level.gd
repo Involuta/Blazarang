3741ebc4bfa7d8b0f5df9d1ff6508342fc814a73
Ryan Bascos <rbascos@uci.edu>
2024-08-27 23:58:07 -0700

    X Dash Teleport Prototype
    When X doesn't have the arm but wants to slipnslice, he recalls the arm, then dashes at the target, then teleports to the center of the arena, then slipnslices at the target. If he already has the arm, he simply slipnslices from his current position. To do: move the icon to his teleport position and make him teleport to places other than the arena center

--- levels/hub_level.gd
+++ levels/hub_level.gd
@@ -1,4 +1,10 @@
 extends Level
 
+@onready var icon := $Target
+
 func _ready():
+	icon.visible = false
 	$UIRoot.awaken()
+	await get_tree().create_timer(2).timeout
+	icon.visible = true
+	icon.global_position = Vector3.UP * 100.0
