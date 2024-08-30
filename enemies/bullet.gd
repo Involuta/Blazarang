5b37804b18e1f2fd1d099592a7e251bc642e3be4
Ryan Bascos <rbascos@uci.edu>
2024-08-30 12:57:05 -0700

    Bullet Particles
    Added particle effects for flying and destruction for normal bullet and mega bullet

--- enemies/bullet.gd
+++ enemies/bullet.gd
@@ -4,17 +4,29 @@ var velocity := Vector3.ONE
 @export var max_lifetime_secs := 4.5
 var invincible := true # allows bullet to go through corners
 var invincibility_secs := .05
+@export var bullet_explosion_secs := 1.0
+var destroyed := false
 
 func _ready():
 	await get_tree().create_timer(invincibility_secs).timeout
 	invincible = false
 	await get_tree().create_timer(max_lifetime_secs).timeout
-	if self:
-		queue_free()
+	if self and not destroyed:
+		destroy_self()
 
 func _physics_process(delta):
 	global_position += velocity*delta
 
+func destroy_self():
+	destroyed = true
+	$EnemyHitbox.queue_free()
+	$BulletMesh.queue_free()
+	$TailParticles.queue_free()
+	$ExplodeParticles.emitting = true
+	print($ExplodeParticles.lifetime)
+	await get_tree().create_timer(bullet_explosion_secs).timeout
+	queue_free()
+
 func _on_body_entered(_body):
 	if not invincible:
-		queue_free()
+		destroy_self()
