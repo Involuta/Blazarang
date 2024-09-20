161756ad672273e94535b6f7344236c6c2bb8ff3
Ryan Bascos <rbascos@uci.edu>
2024-09-19 23:09:26 -0700

    X Health Segment Bugfix
    Bug fixed by adding another health threshold value to the list: 0.0 at index 0. The code as-is functions perfectly with this additional threshold.

--- enemies/boss_hurtbox.gd
+++ enemies/boss_hurtbox.gd
@@ -8,13 +8,13 @@ var recovery_active := false
 
 var damage_indicator_value := 100.0
 
-@export var health_segments := [.25, .5, .75, 1.0]
-var current_health_segment_index := 2
+@export var health_segment_thresholds := [0.0, .25, .5, .75, 1.0]
+var health_segment_threshold_index := 3
 
 func _ready():
 	super()
 	damage_indicator_value = max_health
-	current_health_segment_index = health_segments.size() - 2
+	health_segment_threshold_index = health_segment_thresholds.size() - 2
 
 func reset_recovery_delay():
 	recovery_delay_remaining = recovery_delay
@@ -25,9 +25,9 @@ func receive_hit(damage: float, hitter):
 		damage_indicator_value = health
 	reset_recovery_delay()
 	super(damage, hitter)
-	if health < max_health * health_segments[current_health_segment_index]:
-		current_health_segment_index -= 1
-		print(current_health_segment_index + 1)
+	if health_segment_threshold_index > 0 and health < max_health * health_segment_thresholds[health_segment_threshold_index]:
+		health_segment_threshold_index -= 1
+		print(health_segment_threshold_index + 1)
 
 func _physics_process(delta):
 	super(delta)
@@ -35,7 +35,7 @@ func _physics_process(delta):
 	if recovery_delay_remaining < 0:
 		recovery_active = true
 	if recovery_active:
-		if health < max_health * health_segments[current_health_segment_index + 1]:
+		if health < max_health * health_segment_thresholds[health_segment_threshold_index + 1]:
 			health += recovery_rate
 		if damage_indicator_value > health:
 			damage_indicator_value -= max_health * delta
