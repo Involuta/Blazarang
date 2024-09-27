e495491057bb854ada015ff00e005e5021a85278
Ryan Bascos <rbascos@uci.edu>
2024-09-27 12:44:04 -0700

    X Head Fragment Explosions
    X Head Fragments now leave behind volcano explosions when they disappear. To do: create a warning when the fragments are about to explode

--- enemies/x_head_piece.gd
+++ enemies/x_head_piece.gd
@@ -3,6 +3,7 @@ extends RigidBody3D
 @export var flight_secs := 2.0
 @export var detonate_delay_secs := 12.0
 
+
 func _ready():
 	$FlightParticles.visible = true
 	$BombParticles.visible = false
@@ -10,4 +11,9 @@ func _ready():
 	$FlightParticles.rotation = Vector3(0,0,-PI)
 	$BombParticles.visible = true
 	await get_tree().create_timer(detonate_delay_secs).timeout
+	
+	var volcano_inst = load("res://enemies/x_volcano.tscn").instantiate()
+	$/root/Level.add_child(volcano_inst)
+	volcano_inst.global_position = global_position
+	
 	queue_free()
