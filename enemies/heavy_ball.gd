8aeab50add5b40970bd2ff39b46502e5d94e7c19
Ryan Bascos <rbascos@uci.edu>
2024-12-18 12:53:35 +0800

    Heavy Ball Complete
    Heavy ball now only depresses into the ground upon initial impact when moving at high speeds. If not (e.g. it slides off of another heavy ball already on the ground and gently touches the ground), its entire body stays above the ground and then slowly starts to sink. Its damage is also disabled when it sinks (but not when it slides off of another object)

--- enemies/heavy_ball.gd
+++ enemies/heavy_ball.gd
@@ -2,8 +2,22 @@ extends Ball
 
 var arena_floor_y := 10.0
 @onready var physical_collider = $CollisionShape3D
+@onready var hitbox := $EnemyHitbox
+
+# Min speed necessary for ball to depress into the floor upon initial impact
+# If this speed is not met, the ball starts sinking from its bottom
+@export var depress_min_speed := 10.0
+
+func _ready():
+	hitbox.process_mode = Node.PROCESS_MODE_INHERIT
 
 func _physics_process(delta):
 	super(delta)
-	if global_position.y <= arena_floor_y + physical_collider.shape.radius / 2:
-		linear_velocity = Vector3.ZERO
+	if linear_velocity.length() > depress_min_speed:
+		if global_position.y <= arena_floor_y + physical_collider.shape.radius / 2:
+			linear_velocity = Vector3.ZERO
+			hitbox.process_mode = Node.PROCESS_MODE_DISABLED
+	else:
+		if global_position.y <= arena_floor_y + physical_collider.shape.radius + 1:
+			linear_velocity = Vector3.ZERO
+			hitbox.process_mode = Node.PROCESS_MODE_DISABLED
