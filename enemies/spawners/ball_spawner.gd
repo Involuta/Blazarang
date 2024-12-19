2077a18cf37dca0c406234a735daca2891490533
Ryan Bascos <rbascos@uci.edu>
2024-12-19 22:49:51 +0800

    Vertical Aiming Type
    Mortars and cannons now have their own vertical aiming types: aim at target, aim high at target, and aim at bounce trajectory. Whenever a mortar/cannon chooses a ball to fire, it goes into the firing position for that ball (e.g. goes into bouncer aiming trajectory) for a few secs, then fires. The weapon then has endlag where it stays at the orientation it was at when it shot the ball, preventing it from moving as the ball travels through its barrel.

--- enemies/spawners/ball_spawner.gd
+++ enemies/spawners/ball_spawner.gd
@@ -12,6 +12,12 @@ var skull := preload("res://enemies/skull_ball.tscn")
 var heavy := preload("res://enemies/heavy_ball.tscn")
 
 var lateral_aiming_at_target := true
+enum {
+	AT_TARGET,
+	HIGH_TARGET_TRAJECTORY,
+	HIGH_BOUNCE_TRAJECTORY
+}
+var vert_aim_type := AT_TARGET
 var vert_aiming_at_target := true
 @export var attack_turn_speed := .1
 
@@ -31,8 +37,8 @@ var spawn_cooldown_active := false
 @export var move_speed := 20.0
 @export var move_dir_angle_arc := 30.0
 @export var bounce_height := 20.0
-var high_trajectory_lateral_vel := 0.0
-var high_trajectory_y_vel := 0.0
+var high_target_trajectory_lateral_vel := 0.0
+var high_target_trajectory_y_vel := 0.0
 
 @export var swarm_size := 15.0
 
@@ -41,6 +47,14 @@ var high_trajectory_y_vel := 0.0
 
 @export var heavy_launch_height := 40.0
 @export var arena_floor_y := 10.0
+
+@export var roller_chargeup_secs := 1.0
+@export var bouncer_chargeup_secs := 2.0
+@export var giant_roller_chargeup_secs := 5.0
+@export var giant_bouncer_chargeup_secs := 5.0
+@export var swarm_chargeup_secs := 5.0
+@export var skull_chargeup_secs := 4.5
+@export var heavy_chargeup_secs := 4.0
 
 @onready var mesh = $Mesh
 @onready var root = $/root/ViewControl
@@ -52,22 +66,26 @@ func _ready():
 	target = root.find_child("Icon")
 
 func _physics_process(_delta):
-	update_high_trajectory_vels()
+	update_high_target_trajectory_vels()
 	if self and not spawn_cooldown_active and spawning and not spawn_limit_met():
 		spawn_enemy()
 	if lateral_aiming_at_target:
 		lateral_look_at_target(attack_turn_speed)
-	if vert_aiming_at_target:
-		vert_look_at_target(attack_turn_speed)
-	else:
-		vert_look_high_trajectory(attack_turn_speed)
+	if spawn_cooldown_active:
+		match(vert_aim_type):
+			AT_TARGET:
+				vert_look_at_target(attack_turn_speed)
+			HIGH_BOUNCE_TRAJECTORY:
+				vert_look_high_bounce_trajectory(attack_turn_speed)
+			HIGH_TARGET_TRAJECTORY:
+				vert_look_high_target_trajectory(attack_turn_speed)
 
-func update_high_trajectory_vels():
-	high_trajectory_y_vel = sqrt(2 * gravity * heavy_launch_height + global_position.y)
-	var total_flight_time : float = 2 * high_trajectory_y_vel / gravity
+func update_high_target_trajectory_vels():
+	high_target_trajectory_y_vel = sqrt(2 * gravity * heavy_launch_height + global_position.y)
+	var total_flight_time : float = 2 * high_target_trajectory_y_vel / gravity
 	var vec_to_target := target.global_position - global_position
 	var lateral_dist_to_target := 1.42 * Vector2(vec_to_target.x, vec_to_target.z).length()
-	high_trajectory_lateral_vel = lateral_dist_to_target / total_flight_time
+	high_target_trajectory_lateral_vel = lateral_dist_to_target / total_flight_time
 
 func lateral_look_at_target(turn_speed):
 	var old_rotation := rotation
@@ -85,9 +103,14 @@ func vert_look_at_target(turn_speed):
 	mesh.rotation = old_rotation
 	mesh.rotation.x = lerp_angle(mesh.rotation.x, target_rotation.x, turn_speed)
 
-func vert_look_high_trajectory(turn_speed):
+func vert_look_high_bounce_trajectory(turn_speed):
 	# Rotation "upward" = tan^-1(y_vel/x_vel)
-	var target_rotation_x = atan2(high_trajectory_y_vel, high_trajectory_lateral_vel)
+	var target_rotation_x = atan2(bounce_height, .25 * move_speed)
+	mesh.rotation.x = lerp_angle(mesh.rotation.x, target_rotation_x, turn_speed)
+
+func vert_look_high_target_trajectory(turn_speed):
+	# Rotation "upward" = tan^-1(y_vel/x_vel)
+	var target_rotation_x = atan2(high_target_trajectory_y_vel, high_target_trajectory_lateral_vel)
 	mesh.rotation.x = lerp_angle(mesh.rotation.x, target_rotation_x, turn_speed)
 
 func spawn_limit_met():
@@ -133,6 +156,8 @@ func spawn_from_name(enemy_name):
 			await spawn_roller()
 
 func spawn_roller():
+	vert_aim_type = AT_TARGET
+	await get_tree().create_timer(roller_chargeup_secs).timeout
 	var b = roller.instantiate()
 	level.add_child.call_deferred(b)
 	await b.tree_entered
@@ -142,6 +167,8 @@ func spawn_roller():
 	b.linear_velocity = move_speed * -b.get_global_transform().basis.z
 
 func spawn_bouncer():
+	vert_aim_type = HIGH_BOUNCE_TRAJECTORY
+	await get_tree().create_timer(bouncer_chargeup_secs).timeout
 	var b = bouncer.instantiate()
 	level.add_child.call_deferred(b)
 	await b.tree_entered
@@ -152,6 +179,8 @@ func spawn_bouncer():
 	b.linear_velocity.y = bounce_height
 
 func spawn_giant_roller():
+	vert_aim_type = AT_TARGET
+	await get_tree().create_timer(giant_roller_chargeup_secs).timeout
 	var b = giant_roller.instantiate()
 	level.add_child.call_deferred(b)
 	await b.tree_entered
@@ -160,6 +189,8 @@ func spawn_giant_roller():
 	b.linear_velocity = move_speed * -b.get_global_transform().basis.z
 
 func spawn_giant_bouncer():
+	vert_aim_type = HIGH_BOUNCE_TRAJECTORY
+	await get_tree().create_timer(giant_bouncer_chargeup_secs).timeout
 	var b = giant_bouncer.instantiate()
 	level.add_child.call_deferred(b)
 	await b.tree_entered
@@ -169,6 +200,8 @@ func spawn_giant_bouncer():
 	b.linear_velocity.y = bounce_height
 
 func spawn_swarm():
+	vert_aim_type = AT_TARGET
+	await get_tree().create_timer(swarm_chargeup_secs).timeout
 	for i in range(swarm_size):
 		var b = swarm.instantiate()
 		level.add_child.call_deferred(b)
@@ -180,6 +213,8 @@ func spawn_swarm():
 		b.linear_velocity.y = rng.randfn()
 
 func spawn_skull():
+	vert_aim_type = AT_TARGET
+	await get_tree().create_timer(skull_chargeup_secs).timeout
 	var b = skull.instantiate()
 	level.add_child.call_deferred(b)
 	await b.tree_entered
@@ -189,12 +224,13 @@ func spawn_skull():
 	b.explode_dist = skull_explode_dist
 
 func spawn_heavy():
-	vert_aiming_at_target = false
+	vert_aim_type = HIGH_TARGET_TRAJECTORY
+	await get_tree().create_timer(heavy_chargeup_secs).timeout
 	var b = heavy.instantiate()
 	level.add_child.call_deferred(b)
 	await b.tree_entered
 	b.global_position = global_position
 	b.global_rotation = rotation
-	b.linear_velocity = high_trajectory_lateral_vel * -b.get_global_transform().basis.z
-	b.linear_velocity.y = high_trajectory_y_vel
+	b.linear_velocity = high_target_trajectory_lateral_vel * -b.get_global_transform().basis.z
+	b.linear_velocity.y = high_target_trajectory_y_vel
 	b.arena_floor_y = arena_floor_y
