2fb2e0773bf84f83e175b749dc6a77727e980a5e
Ryan Bascos <rbascos@uci.edu>
2025-01-04 11:22:58 -0800

    Diamond Laser
    Diamond now fires a laser. To do: give laser a hitbox

--- levels/x_boss_level/background_nodes/x_diamond.gd
+++ levels/x_boss_level/background_nodes/x_diamond.gd
@@ -1,11 +1,19 @@
 extends Node3D
 
+@onready var eye := $EyeMesh
 @onready var anim_player := $AnimationPlayer
 
+@onready var root := $/root/ViewControl
+var target : Node3D
+
 func _ready():
+	target = root.find_child("Icon")
 	await get_tree().create_timer(1).timeout
 	anim_player.play("top_twist_out")
 	await anim_player.animation_finished
 	anim_player.play("shoot_laser")
 	await anim_player.animation_finished
 	anim_player.play("top_twist_in")
+
+func _physics_process(_delta):
+	eye.look_at(target.global_position)
