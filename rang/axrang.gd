4e17b3fa9214fc84080df2d30fc7e99d42a0f51b
Ryan Bascos <rbascos@uci.edu>
2025-03-16 11:26:19 -0700

    Ax Throw Direction
    Ax is thrown in the direction Cotu's body is facing

--- rang/axrang.gd
+++ rang/axrang.gd
@@ -4,6 +4,7 @@ var invincible := true
 var invincibility_secs := .5
 
 @export var rotate_speed := .1
+@export var fwd_speed := 1.0
 
 @onready var root := $/root/ViewControl
 var level : Node3D
@@ -18,6 +19,11 @@ func _ready():
 	target = level.find_child("Icon")
 	
 	global_position = target.global_position
+	rotation.y = cotu.armature.rotation.y
+	velocity = fwd_speed * transform.basis.z
 
 func _physics_process(delta):
 	pivot.rotate_x(rotate_speed)
+	var vel_vec = fwd_speed * transform.basis.z
+	move_and_collide(vel_vec, false)
+	#global_position += vel_vec * delta
