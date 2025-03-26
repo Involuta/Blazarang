3f0ee4bb3250a92d86c661b2d7e1d603618fd385
Ryan Bascos <rbascos@uci.edu>
2025-03-25 18:18:31 -0700

    X Laser Combo Ball Special Effects
    Added orange environment glow that grows from the ball when it initially grows, and explosion of particles (slow squares that emanate out from the center and fast streaks) when it does laser overhead

--- enemies/x_laser_combo_ball.gd
+++ enemies/x_laser_combo_ball.gd
@@ -12,6 +12,17 @@ func _on_activate():
 	visible = true
 	for child in get_children():
 		child.process_mode = Node.PROCESS_MODE_INHERIT
+	"""
 	anim_player.play("grow")
 	await get_tree().create_timer(11).timeout
+	anim_player.play("shrink")
+	"""
+	# 10.5 secs btwn grow and shrink
+	anim_player.play("grow")
+	# Laser combo startup is 2.5 secs long
+	await get_tree().create_timer(2.5).timeout
+	# Laser overhead at 4.68 secs after laser combo starts
+	await get_tree().create_timer(2.7).timeout
+	anim_player.play("explosion")
+	await get_tree().create_timer(5.4).timeout
 	anim_player.play("shrink")
