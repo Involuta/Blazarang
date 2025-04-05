6673eb7274bd9b9b6a24c591955df6d01f208f90
Ryan Bascos <rbascos@uci.edu>
2025-04-05 13:42:56 -0700

    Ball Walker Meshes Have Recalculated Normals
    Fixed texture issue where front faces of meshes would be culled due to them being considered the back. Fixed by recalculating normals for each mesh in Blender, then reimporting. To do: make rotation about a single foot possible

--- enemies/ball_walker.gd
+++ enemies/ball_walker.gd
@@ -3,11 +3,9 @@ extends CharacterBody3D
 @export var entity_name := "BallWalker"
 
 enum {
-	WAIT,
-	FOLLOW,
 	ATTACK
 }
-var behav_state = FOLLOW
+var behav_state = ATTACK
 
 @export var aggro_distance := -1
 
@@ -32,7 +30,6 @@ var heavy := preload("res://enemies/heavy_ball.tscn")
 var deathball := preload("res://enemies/death_ball.tscn")
 var popper := preload("res://enemies/popper_ball.tscn")
 
-@onready var nav_agent := $NavigationAgent3D
 #@onready var anim_player := $AnimationPlayer
 #@onready var anim_tree := $AnimationTree
 @onready var root := $/root/ViewControl
@@ -44,103 +41,39 @@ var ball_spawner : Node3D
 func _ready():
 	level = root.find_child("Level")
 	target = root.find_child("Icon")
-	ball_spawner = find_child("BallSpawner")
+	ball_spawner = find_child("BowlPivot")
 	add_to_group("lockonables")
-	if aggro_distance > 0:
-		nav_agent.process_mode = Node.PROCESS_MODE_DISABLED
-		behav_state = WAIT
 
 func _physics_process(delta):
 	if not is_on_floor():
 		velocity.y -= gravity * delta
 	match(behav_state):
-		WAIT:
-			wait()
-		FOLLOW:
-			pass#follow()
 		ATTACK:
-			pass#attack()
+			attack()
 
 func lerp_look_at_target(turn_speed):
 	var vec3_to_target := global_position.direction_to(target.global_position)
-	global_rotation.y = lerp_angle(global_rotation.y, PI + atan2(vec3_to_target.x, vec3_to_target.z), turn_speed)
-
-func lerp_look_at_walk_dir(turn_speed):
-	global_rotation.y = lerp_angle(global_rotation.y, PI + atan2(velocity.x, velocity.z), turn_speed)
-
-func wait():
-	move_and_slide()
-	if global_position.distance_to(target.global_position) < aggro_distance:
-		nav_agent.process_mode = Node.PROCESS_MODE_INHERIT
-		behav_state = FOLLOW
-
-func _on_navigation_agent_3d_target_reached():
-	if behav_state != ATTACK:
-		start_attack()
-
-func _on_navigation_agent_3d_velocity_computed(safe_velocity):
-	if behav_state == FOLLOW:
-		if is_on_floor():
-			# This line accelerates the agent rather than setting its velocity to its desired velocity directly, preventing it from getting caught on corners
-			velocity = velocity.move_toward(safe_velocity, .25)
-		else:
-			# If the enemy is in the air, don't use navigation agent at all
-			var move_dir = global_position.direction_to(target.global_position)
-			velocity.x = follow_speed / 2 * move_dir.x
-			velocity.z = follow_speed / 2 * move_dir.z
-	move_and_slide()
-
-func follow():
-	lerp_look_at_walk_dir(follow_turn_speed)
-	nav_agent.set_target_position(target.global_position)
-	var next_position = nav_agent.get_next_path_position()
-	var new_velocity = (next_position - global_position).normalized() * follow_speed
-	
-	# Sets new wanted velocity, not actual velocity. Wanted velocity is used to compute new safe velocity
-	nav_agent.velocity = new_velocity
-	
-	# If player isn't in sight, reduce target distance to a very small number
-	if can_see_target():
-		nav_agent.target_desired_distance = target_distance
-	else:
-		nav_agent.target_desired_distance = .1
-	
-	#anim_tree.set("parameters/StateMachine/WalkBlendSpace/blend_position", velocity.length())
+	rotation.y = lerp_angle(rotation.y, PI + atan2(vec3_to_target.x, vec3_to_target.z), turn_speed)
 
 func start_attack():
 	behav_state = ATTACK
 	aiming_at_target = true
+	$AnimationPlayer.play("foot_cannon")
 	#anim_tree.set("parameters/StateMachine/conditions/shoot", true)
 
 func try_end_attack():
 	if global_position.distance_to(target.global_position) > target_distance:
 		#anim_tree.set("parameters/StateMachine/conditions/shoot", false)
-		behav_state = FOLLOW
+		pass
 	else:
 		aiming_at_target = true
 
 func attack():
-	nav_agent.velocity.x = 0
-	nav_agent.velocity.z = 0
 	velocity.x = 0
 	velocity.z = 0
 	if aiming_at_target:
 		lerp_look_at_target(attack_turn_speed)
-	ball_spawner.spawning = true
+	#ball_spawner.spawning = true
 
 func stop_aiming_at_target():
 	aiming_at_target = false
-
-func can_see_target():
-	var space_state := get_world_3d().direct_space_state
-	var sight_dir := global_position.direction_to(target.global_position)
-	var query = PhysicsRayQueryParameters3D.create(global_position, global_position + nav_agent.neighbor_distance * sight_dir)
-	query.collision_mask = Globals.make_mask([Globals.ARENA_COL_LAYER, Globals.TARGET_COL_LAYER])
-	query.collide_with_areas = true
-	var result = space_state.intersect_ray(query)
-	if not result:
-		return false
-	if result.collider.collision_layer == Globals.ARENA_COL_LAYER:
-		return false
-	else:
-		return true
