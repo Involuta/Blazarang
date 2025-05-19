4be8f384b49d5ef1123156caf1784e6a24a5ddb5
Ryan Bascos <rbascos@uci.edu>
2025-05-19 01:06:04 -0700

    Save Progress Before MBW Walk Polish
    Added aim at target state and aim at target activation/deactivation funcs

--- enemies/mini_ball_walker.gd
+++ enemies/mini_ball_walker.gd
@@ -9,11 +9,12 @@ var target : Node3D
 @export var walk_moving := true # Whether walker is moving its central ball or not (it's not moving when both feet are on the ground and stationary). Exported so it can be changed in animation
 var moving := true # Whether walker is approaching
 var root_vel := Vector3.ZERO
+var aiming_at_target := true
 
 @export var follow_speed := 3.0
 @export var turn_speed := .1
 @export var kick_dist := 2.0
-@export var kick_secs := 1.5
+@export var kick_secs := 1.833
 
 func _ready():
 	target = root.find_child("Icon")
@@ -25,9 +26,17 @@ func lerp_look_at_target(turn_speed):
 	var dir_to_target := global_position.direction_to(target.global_position)
 	rotation.y = lerp_angle(rotation.y, atan2(dir_to_target.x, dir_to_target.z), turn_speed)
 
+func aim_at_target():
+	aiming_at_target = true
+
+func stop_aiming_at_target():
+	aiming_at_target = false
+
 func _physics_process(delta):
-	if moving:
+	if aiming_at_target:
 		lerp_look_at_target(turn_speed)
+	
+	if moving:
 		var current_rotation = transform.basis.get_rotation_quaternion()
 		velocity = .08 * (current_rotation.normalized() * anim_tree.get_root_motion_position()) / delta
 		velocity.y -= gravity
