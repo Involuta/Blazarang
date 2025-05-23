c6b82e2ca2d2d61b2bd554e1f8788c0e4ff737a2
Ryan Bascos <rbascos@uci.edu>
2025-05-22 21:05:41 -0700

    Added Flash Ball Node
    Flash ball now exists and disappears in 1.5 seconds. To do: add sparking to explosion anim

--- enemies/ball.gd
+++ enemies/ball.gd
@@ -2,9 +2,10 @@ extends RigidBody3D
 class_name Ball
 
 @export var entity_name := "Ball"
+@export var disappear_secs := 30.0
 
 func _ready():
-	await get_tree().create_timer(30).timeout
+	await get_tree().create_timer(disappear_secs).timeout
 	queue_free()
 
 func _physics_process(_delta):
