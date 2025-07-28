56e9e71e1c3c192c9592b7c308f411b24d672539
Ryan Bascos <rbascos@uci.edu>
2025-07-28 14:07:18 -0700

    Deleted Harvestman Middle Limb IK
    To do: try adding extra bones to rig in Blender to fix legs pointing straight up

--- enemies/harvestman_proc_anim_test_mvmt.gd
+++ enemies/harvestman_proc_anim_test_mvmt.gd
@@ -4,23 +4,19 @@ extends Node3D
 
 @export var run_speed := 10.0
 @export var turn_speed := 1.0
-@export var ground_offset := .5 # Height of mite's body from the ground
+@export var ground_offset := .5 # Height of harvestman's body from the ground
 
 # Leg IK targets; note that their positions are also their global positions
-# vf = very front, mb = middle back, mf = middle front, vb = very back
+# vf = very front, vb = very back
 @export var lvf_ik : Marker3D
-@export var rmb_ik : Marker3D
 @export var rvf_ik : Marker3D
-@export var lmb_ik : Marker3D
-@export var lmf_ik : Marker3D
 @export var rvb_ik : Marker3D
-@export var rmf_ik : Marker3D
 @export var lvb_ik : Marker3D
 
 func _process(delta):
 	# Create 2 planes made from the 4 IK targets and get the average of their normals
-	var plane1 = Plane(lmb_ik.position, lvf_ik.position, rvf_ik.position)
-	var plane2 = Plane(rvf_ik.position, rmb_ik.position, lmb_ik.position)
+	var plane1 = Plane(lvb_ik.position, lvf_ik.position, rvf_ik.position)
+	var plane2 = Plane(rvf_ik.position, rvb_ik.position, lvb_ik.position)
 	var avg_normal = ((plane1.normal + plane2.normal) / 2).normalized()
 	
 	# Convert the normal to a basis
@@ -28,7 +24,7 @@ func _process(delta):
 	transform.basis = lerp(transform.basis, target_basis, run_speed * delta).orthonormalized()
 	
 	# Offset body from the ground
-	var avg_ik_pos = (lvf_ik.position + rmb_ik.position + rvf_ik.position + lmb_ik.position + lmf_ik.position + rvb_ik.position + rmf_ik.position + lvb_ik.position) / 8
+	var avg_ik_pos = (lvf_ik.position + rvf_ik.position + rvb_ik.position + lvb_ik.position) / 4
 	var target_pos = avg_ik_pos + transform.basis.y * ground_offset
 	# Dot product gets the difference in positions only in this direction
 	var dist_to_target_pos = transform.basis.y.dot(target_pos - position)
