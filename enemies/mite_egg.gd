df239b37d46555f9bc886e801b8ed380f4c7de75
Ryan Bascos <rbascos@uci.edu>
2025-09-20 16:12:39 -0700

    Made Mite Egg Scene
    Mite eggs fall to the ground and disappear after 1 second of being grounded. To do: make egg spawn mites after a landing

---
+++ enemies/mite_egg.gd
@@ -0,0 +1,46 @@
+extends Node3D
+
+var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+var velocity := Vector3.ONE
+@export var max_lifetime_secs := 9.0
+var invincible := true # prevents bullet from hitting self
+var invincibility_secs := .05
+@export var bullet_explosion_secs := 1.0
+var destroyed := false
+
+@onready var flight_particles := $FlightParticles
+@onready var impact_particles := $ImpactParticles
+
+func _ready():
+	flight_particles.emitting = true
+	impact_particles.emitting = false
+	await get_tree().create_timer(invincibility_secs).timeout
+	invincible = false
+	await get_tree().create_timer(max_lifetime_secs).timeout
+	if not destroyed and self:
+		destroy_self()
+
+func _physics_process(delta):
+	if not destroyed:
+		global_position += velocity * delta
+		velocity.y -= gravity * delta
+
+func _on_body_entered(body):
+	if invincible:
+		return
+	
+	# Prevents collision with ground webs and paramites; all non-paramites are thick enemies
+	if Globals.compare_layers(body.collision_layer, Globals.ENEMY_COL_LAYER):
+		pass
+	elif Globals.compare_layers(body.collision_layer, Globals.ARENA_COL_LAYER):
+		destroy_self()
+
+func destroy_self():
+	$AnimationPlayer.play("explode")
+	flight_particles.emitting = false
+	impact_particles.emitting = true
+	destroyed = true
+	# For whatever reason, high velocity apparently makes the particles disappear early
+	velocity = Vector3.ZERO
+	await get_tree().create_timer(bullet_explosion_secs).timeout
+	queue_free()
