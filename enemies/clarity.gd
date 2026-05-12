d68a5785e9b96e2a1611cbd223d2f9c2a229fadf
Ryan Bascos <rbascos@uci.edu>
2026-05-11 23:27:20 -0700

    Long Slice Longer Startup
    Long slice is now telegraphed to a similar degree as double slice

--- enemies/clarity.gd
+++ enemies/clarity.gd
@@ -199,8 +199,6 @@ func switch_to_staggered():
 	await get_tree().create_timer(frames(196)).timeout
 	# Return to normal anim tree/state machine behavior
 	switch_to_circling()
-	arm_state_machine.travel("WalkLeftAggressive")
-	body_state_machine.travel("WalkLeftAggressive")
 
 func set_head_rotation(rot_deg: Vector3):
 	# Set rotation of dynamic head (the head that looks at the player)
@@ -585,7 +583,7 @@ func contract_blizzard_safezone(frame_duration: int):
 	t.tween_property(level_env, "volumetric_fog_emission", Color("#65768f"), frames(frame_duration))
 	#t.tween_property(particle_attractor, "strength", 0, frames(frame_duration/2))
 	# Body fog
-	t.tween_property(body_cone_fog.material, "shader_parameter/density", 0.3, frames(frame_duration/2.0))
+	t.tween_property(body_cone_fog.material, "shader_parameter/density", 0.3, frames(frame_duration/2))
 	#t.tween_property(feet_fog.material, "shader_parameter/density", 0.15, frames(frames/2))
 
 	await get_tree().create_timer(frames(frame_duration)).timeout
