d921f01f04ad63dcb753530e79edcb8eb041d61d
Ryan Bascos <rbascos@uci.edu>
2026-07-23 09:50:47 -0700

    Infuse Slice Ring Grows From Both Directions
    Ring grows both counterclockwise and clockwise from the starting point

--- enemies/clarity_infused_slice_ring.gd
+++ enemies/clarity_infused_slice_ring.gd
@@ -3,7 +3,7 @@ extends Node3D
 var appear_time := .3
 var bright_time := 4.5 # Time ring spends at full brightness
 var fade_time := 4.5 # Time ring spends fading to nothing
-var disappear_time := 6.0 # Time spent waiting for particles to disappear after ring disappears
+var disappear_time := 2.0 # Time spent waiting for particles to disappear after ring disappears
 @onready var ring_mesh := $RingMesh
 @onready var cloud_particles := $CloudParticles
 
@@ -13,9 +13,9 @@ func _ready():
 func ring_animation():
 	var mat = ring_mesh.mesh.material as ShaderMaterial
 	var t = create_tween()
-	t.tween_property(mat, "shader_parameter/progress", 1.0, appear_time).from(0.0).set_ease(Tween.EASE_IN_OUT)
+	t.tween_property(mat, "shader_parameter/progress", 0.6, appear_time).from(0.0).set_ease(Tween.EASE_IN_OUT)
 	t.tween_interval(bright_time)
-	t.tween_property(mat, "shader_parameter/fade_alpha", 0.0, fade_time).from(1.0)
 	t.tween_property(cloud_particles, "emitting", false, 0.0)
+	t.tween_property(mat, "shader_parameter/fade_alpha", 0.0, fade_time).from(1.0)
 	t.tween_interval(disappear_time)
 	t.tween_callback(queue_free)
