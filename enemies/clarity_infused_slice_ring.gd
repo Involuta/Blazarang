302bc4f9ab970a5e28d0aae216660f480031a677
Ryan Bascos <rbascos@uci.edu>
2026-07-23 15:12:57 -0700

    Fixed Bug Where Ring Doesn't Appear on Second Infused Slice
    Fixed by resetting fade alpha at beginning of ring anim

--- enemies/clarity_infused_slice_ring.gd
+++ enemies/clarity_infused_slice_ring.gd
@@ -1,6 +1,6 @@
 extends Node3D
 
-var appear_time := .3
+var appear_time := .2
 var bright_time := 4.5 # Time ring spends at full brightness
 var fade_time := 4.5 # Time ring spends fading to nothing
 var disappear_time := 2.0 # Time spent waiting for particles to disappear after ring disappears
@@ -13,9 +13,14 @@ func _ready():
 func ring_animation():
 	var mat = ring_mesh.mesh.material as ShaderMaterial
 	var t = create_tween()
+	# Reset fade_alpha so that when it always starts out visible
+	t.tween_property(mat, "shader_parameter/fade_alpha", 1.0, 0.0)
 	t.tween_property(mat, "shader_parameter/progress", 0.6, appear_time).from(0.0).set_ease(Tween.EASE_IN_OUT)
 	t.tween_interval(bright_time)
 	t.tween_property(cloud_particles, "emitting", false, 0.0)
 	t.tween_property(mat, "shader_parameter/fade_alpha", 0.0, fade_time).from(1.0)
 	t.tween_interval(disappear_time)
-	t.tween_callback(queue_free)
+	t.tween_callback(destroy_self)
+
+func destroy_self():
+	queue_free()
