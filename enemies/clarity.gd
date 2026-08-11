117fe2af90a9305f52aa91c756a0fce6b2dc05ec
Ryan Bascos <rbascos@uci.edu>
2026-08-11 08:49:44 -0700

    QuadFanShardSequence 1 & 2

--- enemies/clarity.gd
+++ enemies/clarity.gd
@@ -778,7 +778,7 @@ func play_anim_all_dress_shards(s: String):
 		if s == "JumpShotToWalkLeft":
 			ds.anim_player.play(s)
 		else:
-			ds.anim_player.play("QuadShardSemicircleSequence2")
+			ds.anim_player.play("QuadShardFanSequence1")
 
 func snowflake_brighten(brightness: float, duration: float):
 	var t = get_tree().create_tween()
